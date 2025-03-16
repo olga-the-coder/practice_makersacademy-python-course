6a6c56a3c5da1f32356e55d924415dbfc7826744
olga-the-coder <olga.tabalina@gmail.com>
2025-03-16 13:40:18 +0000

    Add numbers function and python operators

---
+++ 015_add_numbers.py
@@ -0,0 +1,13 @@
+# @TASK: Now you try. Here's an exercise for you:
+#
+# Write a function called `add_numbers` that:
+#
+# * Takes two numbers as input
+# * Adds them together
+# * Returns the result
+
+def add_numbers(number1, number2):
+    return number1 + number2
+
+print("add_numbers(5,9) is:")
+print(add_numbers(5,9))
---
+++ 016_operators.py
@@ -0,0 +1,54 @@
+def add_one(num):
+  return num + 1
+
+# You may need to widen the panel or zoom out to see the table:
+
+# | Code           | What is it?                                        |
+# | -------------- | -------------------------------------------------- |
+# | def            | `def` is a keyword that defines a new function     |
+# | add_one        | `add_one` is the function name                     |
+# | (num)          | `(num)` is the parameter list                      |
+# | num            | `num` is a parameter                               |
+# | :              | The `:` symbol indicates the body should start now |
+# | return num + 1 | `return num + 1` is a statement                    |
+# | num + 1        | `num + 1` is an expression                         |
+# | num            | `num` here is a variable                           |
+# | +              | `+` is an operator                                 |
+# | 1              | `1` is a literal number                            |
+
+# @TASK: To be a great programmer you will have to become a great researcher.
+# Let's get started:
+#
+# 1. Search the web for "Python operators", then...
+# 2. Find and fill out the following list of operators.
+#
+# I've started it for you.
+
+# Addition
+added = 2 + 3
+print(f"2 + 3 = {added}")
+
+# Multiplication
+multiplied = 2 * 3
+print(f"2 * 3 = {multiplied}")
+
+# Subtraction
+subtracted = 2 - 3
+print(f"2 - 3 = {subtracted}")
+
+#Division
+divided = 2 / 3
+print(f"2 / 3 = {divided}")
+
+#Modulus
+modulus = 2 % 3
+print(f"2 % 3 = {modulus}")
+
+#Floor Division
+floor_divided = 2 // 3
+print(f"2 // 3 = {floor_divided}")
+
+# == Exponentiation == Sometimes known as "2 to the power of 3"
+exp = 2 ** 3
+print(f"2 ** 3 = {exp}")
+
