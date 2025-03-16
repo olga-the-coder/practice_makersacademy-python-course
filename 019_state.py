803ad887b792a38a57056c8b6e185f309fa3977a
olga-the-coder <olga.tabalina@gmail.com>
2025-03-16 14:46:24 +0000

    practice python statements

---
+++ 019_state.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+a = b
+print(f"a is {a}")
+print(f"a is {b}")
+
+#a = 10: This line assigns the value 10 to the variable a.
+
+#b = 20: This line assigns the value 20 to the variable b.
+
+#a = b: This line makes the value of a equal to the value of b. So, a now holds the value 20, because b holds 20.
+
+#print(f"a is {a}"): This is a formatted string (also called an f-string). It prints the value of a. Since we just set a to be 20 in the previous step, it will print: a is 20
+
+#print(f"b is {b}"): This also prints the value of b. Since b is still 20 (as we assigned it earlier), it will print: b is 20
---
+++ 021_two_step.py
@@ -0,0 +1,52 @@
+def add_one_and_divide_by_two_with_statements(num):
+    added = num + 1
+    halved = added / 2
+    return halved
+
+print("add_one_and_divide_by_two_with_statements(5) is:")
+print(add_one_and_divide_by_two_with_statements(5))
+
+def add_one_and_divide_by_two_with_an_expression(num):
+  return (num + 1) / 2
+
+print("add_one_and_divide_by_two_with_an_expression(5) is:")
+
+print(
+  add_one_and_divide_by_two_with_an_expression(5)
+)
+
+# @TASK: Complete these functions.
+
+# == Exercise One ==
+
+print("")
+print("Function: divide_by_two_and_add_one")
+
+def divide_by_two_and_add_one(num):
+    return num / 2 + 1
+
+print("divide_by_two_and_add_one(8) is:")
+print(divide_by_two_and_add_one(8))
+
+# == Exercise Two ==
+
+print("")
+print("Function: multiply_by_forty_and_add_sixty")
+
+def multiply_by_forty_and_add_sixty(num):
+    return num * 40 + 60
+
+print("multiply_by_forty_and_add_sixty(3423) is:")
+print(multiply_by_forty_and_add_sixty(3423))
+
+# == Exercise Three ==
+
+print("")
+print("Function: add_together_and_double")
+
+def add_together_and_double(num_a, num_b):
+    return (num_a + num_b) * 2
+
+print("add_together_and_double(3, 4) is:")
+print(add_together_and_double(3, 4))
+
